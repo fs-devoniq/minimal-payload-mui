480378e96d19843b6d9202d35b1998d049f848e8
Fiete Scheel <fscheel@devoniq.de>
2026-04-14 08:48:45 +0200

    feat: media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -12,5 +12,55 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
-  upload: true,
+  upload: {
+    staticDir: 'uploads/media',
+    mimeTypes: ['image/*'],
+    formatOptions: {
+      format: 'webp',
+      options: {
+        quality: 100,
+      },
+    },
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 480,
+        height: 320,
+        withoutEnlargement: true,
+        formatOptions: {
+          format: 'webp',
+          options: {
+            quality: 100,
+          },
+        },
+      },
+      {
+        name: 'medium',
+        width: 960,
+        height: 640,
+        withoutEnlargement: true,
+        formatOptions: {
+          format: 'webp',
+          options: {
+            quality: 100,
+          },
+        },
+      },
+      {
+        name: 'large',
+        width: 1600,
+        height: 1067,
+        withoutEnlargement: true,
+        formatOptions: {
+          format: 'webp',
+          options: {
+            quality: 100,
+          },
+        },
+      },
+    ],
+    adminThumbnail: 'thumbnail',
+    focalPoint: true,
+    crop: true,
+  },
 }
